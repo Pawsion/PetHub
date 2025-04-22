ab712fa969d030c566735c23f111b3ea8555899b
UrosGruja <urosgrujicicfax@gmail.com>
2025-04-22 14:31:08 +0200

    Kennels LIst

--- src/kennels/kennels.controller.ts
+++ src/kennels/kennels.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateKennelDto } from './createKennel.dto';
 import { Kennel } from './kennels.entity';
 import { KennelsService } from './kennels.service';
@@ -12,10 +12,20 @@ export class KennelsController {
     async create(@Body() dto: CreateKennelDto): Promise<Kennel>{
         return await this.kennelsService.create(dto);
     }
-    // @Get()
-    // async findAll(): Promise<Kennel[]>{
-    //     return await this.kennelsService.findAll();
-    // }
+    @Get()
+async findAll(
+    @Query('location') location?: string,
+    @Query('minPrice') minPrice?: number,
+    @Query('maxPrice') maxPrice?: number,
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query('sortBy') sortBy: 'price_per_night' | 'name' | 'created_at' = 'price_per_night',
+    @Query('sortOrder') sortOrder: 'ASC' | 'DESC' = 'ASC',
+  ) {
+    return this.kennelsService.findAll(location, minPrice, maxPrice, page, limit, sortBy, sortOrder);
+  }
+
+    
     // @Get(':id')
     // async findOne(@Body() id: number): Promise<Kennel>{
     //     return await this.kennelsService.findOne(id);
